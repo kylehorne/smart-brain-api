57a79f6461c1f535e44922a2892900fcc8f66c62
kylehorne <kylehorne.b@gmail.com>
2020-08-04 11:14:59 -0700

    updating database again

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host: 'postgresql-concentric-39286',
-    user: 'kylehorne',
-    password: '',
-    database: 'smart-brain'
+    host: process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
